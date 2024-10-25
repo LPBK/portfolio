5005feb7ec5100c9c188dd3dbcc8d4ea55fd97b7
LPB <130961341+LPBK@users.noreply.github.com>
2024-10-25 02:11:58 -0400

    2 steps
    uploading the 2 steps code generator

---
+++ practica6/index.js
@@ -0,0 +1,4 @@
+const container=document.getElementById("container")
+const inputEl=document.getElementById("input-field").value+="CODE = "+Math.floor(100 + Math.random() * 90)+"-"+Math.floor(100 + Math.random() * 90)
+
+
