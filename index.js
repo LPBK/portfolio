4b1e052e43df866e33faf4ac2eac9e74885c0316
LPB <130961341+LPBK@users.noreply.github.com>
2024-10-25 02:10:03 -0400

    Delete index.js

--- index.js
+++
@@ -1,4 +0,0 @@
-const container=document.getElementById("container")
-const inputEl=document.getElementById("input-field").value+="CODE = "+Math.floor(100 + Math.random() * 90)+"-"+Math.floor(100 + Math.random() * 90)
-
-
