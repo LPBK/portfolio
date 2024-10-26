c401c5075fe270fdfef0b852a0f6fb0fa26a8270
LPB <130961341+LPBK@users.noreply.github.com>
2024-10-26 13:11:49 -0400

    Update index.js
    added the password generator

--- practica6/index.js
+++ practica6/index.js
@@ -1,10 +1,16 @@
+//se declaran las constantes y variables.
 const container=document.getElementById("container")
 let numero=60
 const inputEl=document.getElementById("input-field")
+const generatebtn=document.getElementById("generate");
+const symbols="abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()_+~`|}{[]:;?><,./-=";
+//esta funcion genera codigos random para el authenticator
 const generarcodigo=()=>{
     return "CODE = " + Math.floor(100 + Math.random() * 90) + "-" + Math.floor(100 + Math.random() * 90);
 };
+//se le asigna proyeccion al input
 inputEl.value=generarcodigo()
+//se declara la variable de cambio con el tiempo
 const cambiar=() => {
     if(numero>=0){
         numero--;
@@ -15,4 +21,18 @@ const cambiar=() => {
         numero=60
     } 
 };
+//se setea un intervalo a 1000 para un conteo regulado
 setInterval(cambiar, 1000);
+//se crea el mix de arrays
+function mixear() {
+    let password = "";
+    let length=document.getElementById("lenght").value
+    for (let i = 0; i<length; i++) {
+        const randomIndex = Math.floor(Math.random() * symbols.length);
+        password += symbols[randomIndex];
+    }
+    document.getElementById("passwordptn").innerText = password;
+}
+
+
+
